59b43ce1b1c7cba8cc40b54b05168e0ad4abb554
David Bezuidenhout <tinuva@ipv6.za.org>
2024-08-05 10:37:27 +0200

    fix missing leading 0s in ipv6 dns

--- network/router/files/eui64.sh
+++ network/router/files/eui64.sh
@@ -13,6 +13,12 @@ mac_to_ipv6_ll() {
     echo "$(printf %02x $((0x$1 ^ 2)))$2:${3}ff:fe$4:$5$6"
 }
 
-IPV6=$(mac_to_ipv6_ll $MAC)
+mac_to_eui64() {
+ IFS=':'; set $1; unset IFS
+ echo "$(printf %x $((0x$1 ^ 2)))$2:${3}ff:fe$4:$5$6" |
+  sed -E 's/:0+/:/g; s/:{3,}/::/; s/:$/:0/'
+}
+
+IPV6=$(mac_to_eui64 $MAC)
 
 jq -n --arg ipv6 "$IPV6" '{"ipv6":$ipv6}'
